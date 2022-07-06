d38f015f20c2c6c5bd345c5b9ab5908bdb70e357
jeanelamyca <jeanelamyca.molino@g.batstate-u.edu.ph>
2022-07-06 20:20:31 +0800

    feat: added endpoints and necessary functions to handle student related procedures

---
+++ app/students/routes.py
@@ -0,0 +1,24 @@
+from flask import Blueprint
+from flask import jsonify, make_response, request
+from app.students.controllers import StudentManager
+from flask import url_for
+
+module = Blueprint("students", __name__)
+
+
+@module.route("/students", methods=["GET"])
+def get_students():
+    res = StudentManager.get_students()
+    return make_response(res)
+
+
+@module.route("/students", methods=["POST"])
+def add_students():
+    response = StudentManager.create_student(request.form)
+    return make_response(jsonify(response))
+
+
+@module.route("/students/<id>", methods=["GET"])
+def get_student_by_id(id):
+    res = StudentManager.get_student_by_id(id)
+    return make_response(res)
